a654165f07e1a0b39712335b12410a5de9ccae78
Bharat Poptwani <b4bharat.available@gmail.com>
2016-10-13 13:15:29 +0530

    basic promise flow explained

--- basicPromiseEx.js
+++ basicPromiseEx.js
@@ -6,6 +6,7 @@ const request = require('request');
 console.log('*****************************************************');
 console.log('Basic Promise Example');
 console.log('*****************************************************');
+//'fetch' returns a promise object, if the promise is resolved, it returns data [response.status] in our case, if a promise is rejected, it returns a reason for rejection.
 fetch('http://httpbin.org/get')
 .then(response => {
   console.log('Request using Promises done. Response status code: %d', response.status);
